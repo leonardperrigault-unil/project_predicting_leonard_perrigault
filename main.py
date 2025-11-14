f9d4ecd2e9eb9ee146ecb9bfaff4204ba4111498
leonardperrigault-unil <leonardperrigault@gmail.com>
2025-11-14 12:42:08 +0100

    feat: Create a parser for the main
    3 ways to use it for the moment such clean train and test

--- main.py
+++ main.py
@@ -1,3 +1,30 @@
+import argparse
 from src.data_cleaning import clean
 
-clean.main()
+def main():
+    parser = argparse.ArgumentParser(
+        description='ML Project - World Data Analysis',
+        formatter_class=argparse.RawDescriptionHelpFormatter
+    )
+
+    parser.add_argument(
+        '-m', '--module',
+        type=str,
+        required=True,
+        choices=['clean','train', 'test'],
+        help='Module to run: clean, train, test'
+    )
+
+    args = parser.parse_args()
+
+    if args.module == 'clean':
+        clean.main(input_file=args.input, output_file=args.output)
+    elif args.module == 'train':
+        print("t")
+    elif args.module == 'test':
+        print("t")
+    else:
+        print(f"Unknown module: {args.module}")
+
+if __name__ == "__main__":
+    main()
